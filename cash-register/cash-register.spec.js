71dbc9044ba2c26f94ad6643d00925dd8d1e9bd1
Pallav Jain <pallavjain409@gmail.com>
2019-06-14 14:55:17 +0530

    formatting of cash-register.spec.js

--- cash-register/cash-register.spec.js
+++ cash-register/cash-register.spec.js
@@ -1,21 +1,106 @@
 var checkCashRegister = require("./cash-register.js");
-test("OPEN STATUS 1", function(){
-  const actualAnswer = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-  expect(actualAnswer).toStrictEqual({status: "OPEN", change: [["QUARTER", 0.5]]})
+test("OPEN STATUS 1", function() {
+  const actualAnswer = checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ]);
+  expect(actualAnswer).toStrictEqual({
+    status: "OPEN",
+    change: [
+      ["QUARTER", 0.5]
+    ]
+  })
 });
-test("OPEN STATUS  2", function(){
-  const actualAnswer = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-  expect(actualAnswer).toStrictEqual({status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]});
+test("OPEN STATUS  2", function() {
+  const actualAnswer = checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ]);
+  expect(actualAnswer).toStrictEqual({
+    status: "OPEN",
+    change: [
+      ["TWENTY", 60],
+      ["TEN", 20],
+      ["FIVE", 15],
+      ["ONE", 1],
+      ["QUARTER", 0.5],
+      ["DIME", 0.2],
+      ["PENNY", 0.04]
+    ]
+  });
 })
-test("CLOSED STATUS", function(){
-  const actualAnswer = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-  expect(actualAnswer).toStrictEqual({status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]});
+test("CLOSED STATUS", function() {
+  const actualAnswer = checkCashRegister(19.5, 20, [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]);
+  expect(actualAnswer).toStrictEqual({
+    status: "CLOSED",
+    change: [
+      ["PENNY", 0.5],
+      ["NICKEL", 0],
+      ["DIME", 0],
+      ["QUARTER", 0],
+      ["ONE", 0],
+      ["FIVE", 0],
+      ["TEN", 0],
+      ["TWENTY", 0],
+      ["ONE HUNDRED", 0]
+    ]
+  });
 });
-test("INSUFFICIENTS FUNDS", function(){
-  const actualAnswer = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-  expect(actualAnswer).toStrictEqual({status: "INSUFFICIENT_FUNDS", change: []});
+test("INSUFFICIENTS FUNDS", function() {
+  const actualAnswer = checkCashRegister(19.5, 20, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]);
+  expect(actualAnswer).toStrictEqual({
+    status: "INSUFFICIENT_FUNDS",
+    change: []
+  });
 })
-test("INSUFFICIENTS FUNDS 2", function(){
-  const actualAnswer = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-  expect(actualAnswer).toStrictEqual({status: "INSUFFICIENT_FUNDS", change: []});
+test("INSUFFICIENTS FUNDS 2", function() {
+  const actualAnswer = checkCashRegister(19.5, 20, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 1],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]);
+  expect(actualAnswer).toStrictEqual({
+    status: "INSUFFICIENT_FUNDS",
+    change: []
+  });
 })
