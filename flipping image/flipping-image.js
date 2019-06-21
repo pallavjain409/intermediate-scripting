ebc4973e7f62029610e6a0716d85b451172760a0
Pallav Jain <pallavjain409@gmail.com>
2019-06-21 08:19:14 +0530

    function invert modified

--- flipping image/flipping-image.js
+++ flipping image/flipping-image.js
@@ -1,18 +1,15 @@
 var flipAndInvertImage = function(A) {
- return  A.map(arr => flipRow(arr).map(input => invert(input)));
+  return A.map(arr => flipRow(arr).map(input => invert(input)));
 };
-function flipRow(arr){
-    let A = [];
-    while(arr.length){
-        A.push(arr.pop());
-    }
-    return A;
+
+function flipRow(arr) {
+  let A = [];
+  while (arr.length) {
+    A.push(arr.pop());
+  }
+  return A;
 }
-function invert(input){
-    if(input == 0){
-        return 1;
-    }
-    else{
-        return 0;
-    }
+
+function invert(input) {
+  return input == 0 ? 1 : 0;
 }
