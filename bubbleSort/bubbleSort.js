f286930549a9403b52e9cf4cc1ecd2d82bc779b7
Pallav Jain <pallavjain409@gmail.com>
2019-07-07 13:09:06 +0530

    bubblesort modified

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -5,10 +5,8 @@ function bubbleSort(array) {
         return array;
     }
     do {
-        outerCount++;
         swapped = false;
         for (let count = 0; count < array.length; count++) {
-            innerCount++
             if (array[count] > array[count + 1]) {
                 array = swapArrayItem(array, count, count + 1);
                 swapped = true;
@@ -16,13 +14,10 @@ function bubbleSort(array) {
         }
     }
     while (swapped);
-    console.log(`innerCount: ${innerCount} outerCount: ${outerCount} swapCount ${swapCount}`)
     return array;
 }
 function swapArrayItem(array, index1, index2) {
-    let temp;
-    swapCount++;
-    temp = array[index1];
+    let temp = array[index1];
     array[index1] = array[index2];
     array[index2] = temp;
     return array;
--- bubbleSort/bubbleSort.spec.js
+++ bubbleSort/bubbleSort.spec.js
@@ -1,9 +1,9 @@
 var bubbleSort = require("./bubbleSort.js")
-test("Array1", function(){
-    const actualAnswer = bubbleSort([32,54,2,53,22]);
-    expect(actualAnswer).toBe([2,22,32,53,54]);
+test("Array1", function () {
+    const actualAnswer = bubbleSort([32, 54, 2, 53, 22]);
+    expect(actualAnswer).toStrictEqual([2, 22, 32, 53, 54]);
 })
-test("Array2", function(){
-    const actualAnswer = bubbleSort([4,453,223,33,32,14,565,3]);
-    expect(actualAnswer).toBe(3,4,14,32,33,223,453,565);
+test("Array2", function () {
+    const actualAnswer = bubbleSort([4, 453, 223, 33, 32, 14, 565, 3]);
+    expect(actualAnswer).toStrictEqual([3, 4, 14, 32, 33, 223, 453, 565]);
 })
