6dd0074cccc2a0292b93263f9c98a8d729609426
Pallav Jain <pallavjain409@gmail.com>
2019-07-07 16:56:57 +0530

    binarySearch modified

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -2,7 +2,7 @@ function binarySearch(array, target) {
   let end = array.length - 1;
   let start = 0;
   while (start <= end) {
-    let middile = Math.floor((start + end) / 2)
+    let middle = Math.floor((start + end) / 2)
     if (array[middle] == target) {
       return middle
     }
