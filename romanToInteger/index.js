209d18da9c7a4ab64de46328e9cf8dfd57a2bb32
Pallav Jain <pallavjain409@gmail.com>
2019-09-19 08:23:30 +0530

    romanToInteger

--- romanToInteger/index.js
+++ romanToInteger/index.js
@@ -8,23 +8,20 @@ var romanToInt = function (s) {
         D: 500,
         M: 1000
     };
-    let count = s.length - 1; //1
+    let count = s.length - 1;
     let int = 0;
-    let prev = roman[s[count]]; //5
+    let prev = roman[s[count]]; 
 
     while (count >= 0) {
         let curr = roman[s[count]];
         if (curr >= prev) {
             int += curr;
         } else {
-            if (int <= curr) {
-                int = curr - int;
-            } else {
-                int = int - curr;
-            }
+            int = int - curr;
         }
         prev = curr;
         count--;
     }
     return int;
 };
+console.log(romanToInt("LVIII"))
