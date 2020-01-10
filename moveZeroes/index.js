e1045a22423db432344db7da109f16bb9b509a74
Pallav Jain <pallavjain409@gmail.com>
2020-01-10 16:31:12 +0530

    Add another solution for move zeroes

--- moveZeroes/index.js
+++ moveZeroes/index.js
@@ -16,3 +16,21 @@ var moveZeroes = function (nums) {
     }
     return nums;
 };
+
+
+// another solution move zeroes
+
+var moveZeroes = function(nums) {
+    let count = 0
+   nums.forEach((_, index)=> {
+       if (nums[index] !== 0){
+           nums[count] = nums[index]
+           count++
+       } })
+      for (let i = count; i < nums.length; i ++){
+          nums[i] = 0
+      }
+
+   return nums
+
+};
