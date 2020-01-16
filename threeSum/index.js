9ea976cc8799f8a5d2a1de4f9ce9478715695c44
Pallav Jain <pallavjain409@gmail.com>
2020-01-16 17:39:00 +0530

    threeSUm

--- threeSum/index.js
+++ threeSum/index.js
@@ -1,12 +1,34 @@
-var threeSum = function(nums){
-    let map = new Map()
-    for (let i = 0 ; i < nums.length; i++){
-        for (let j = i + 1; j < nums.length ; j++){
-            let sumOfTwo = nums[i] + nums[j]
-            map.set(sumOfTwo, [nums[i], nums[j]])
+var threeSum = function(arr){
+    let result = [];
+    arr = arr.sort((a, b) => a-b)
+    for (let i = 0; i < arr.length - 3; i++){
+        if(i == 0 || arr[i] > arr[i - 1]){ // [-4, -1, -1, 0 ,1, 2]
+            let start = i + 1; // 1
+            let end = arr.length - 1; // 5
+            while(start < end){ // 3 // 5
+                if (arr[i] + arr[start] + arr[end] === 0){
+                   // -4 -1 -1 = -6
+                    result.push([arr[i], arr[start], arr[end]])
+                    start++ ;
+                    end--
+                }
+                else if (arr[i] + arr[start] + arr[end] < 0){
+                  
+                   let current = arr[start]
+                   while(current === arr[start] && start < end){
+                       start++
+                   }
+                }
+                 if (arr[i] + arr[start] + arr[end] > 0){
+                    let current = arr[end]
+                    while(current === arr[end] && start < end){
+                        end--
+                    }
+                 }
+            }
         }
     }
-    console.log(map)
+    return result
 }
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4]))
+console.log(threeSum([0,0,0,]))
