d08b2d5f1f222fad7f6eb62f1d395f126bbe9289
Pallav Jain <pallavjain409@gmail.com>
2020-01-21 11:46:16 +0530

    some more point

--- sortByAbosluteValue/index.js
+++ sortByAbosluteValue/index.js
@@ -50,5 +50,54 @@ const sortByAbsoluteValue2 = function(arr) {
     return arr;
   };
   
-  let arr = [0, -2, 2, -4, -1, -2]; // [ 0, -1, -2, -2, 2, -4 ]
-  console.log(sortByAbsoluteValue2(arr));
+Array.prototype.quickSort = function (comparator = (a, b) => a <= b, lb = 0, ub = this.length - 1) {
+  let arr = this;
+  if (lb < ub) {
+      let pivotIndex = partion(arr, lb, ub, comparator);
+      arr.quickSort(comparator, lb, pivotIndex - 1);
+      arr.quickSort(comparator, pivotIndex + 1, ub)
+  }
+  return arr;
+
+  function partion(arr, lb, ub, comparator) {
+      let start = lb;
+      let end = ub;
+      let pivot = arr[lb];
+      while (start < end) {
+          while (comparator(arr[start], pivot)) {
+              if (start >= ub) {
+                  break;
+              } else {
+                  start++
+              }
+          }
+          while (!comparator(arr[end], pivot)) {
+              end--;
+          }
+          if (start < end) {
+              [arr[start], arr[end]] = [arr[end], arr[start]]
+
+          }
+      }
+      [arr[end], arr[lb]] = [arr[lb], arr[end]];
+
+      return end;
+  }
+}
+
+const sortByAbsoluteValue3 = function(arr) {
+
+  arr.quickSort((a, b) => {
+    if (absolute(a) == absolute(b)) {
+      return a <= b;
+    } else {
+      return absolute(a) <= absolute(b);
+    }
+  });
+
+  return arr;
+};
+
+
+let arr = [0, -2, 2, -4, -1, -2]; // [ 0, -1, -2, -2, 2, -4 ]
+console.log(sortByAbsoluteValue3(arr));
